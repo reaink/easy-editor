fc132431077ca9b82987defdebf44e323a9fc0ad
不书 <nmlixa@163.com>
2018-10-23 17:27:07 +0800

    同步最新代码

--- easy-editor/easy-editor.js
+++ easy-editor/easy-editor.js
@@ -11,40 +11,69 @@ EasyEditor.prototype = {
     for (let item of Object.entries(obj)) {
       this.data[item[0]] = item[1];
     }
-    this.el = document.querySelector(this.data.el);
-    //设置编辑区
-    this.el.addEventListener('mousedown', () => {
-      //创建内容区
-      this.editor = this.creEl('div');
-      this.editor.setAttribute('contentEditable', 'true');
-      this.editor.setAttribute('class', 'es-line');
-      this.el.appendChild(this.editor);
-      this.editCont(this.editor);
-      this.reSet(this.editor);
+    //获取 并 设置editor
+    this.editor = this.seEl(this.data.el);
+    this.editor.style.display = 'none';
+
+    //创建并插入iframe
+    this.iframe = this.creEl('iframe');
+    this.iframe.style.width = this.data.width;
+    this.iframe.style.height = this.data.height;
+    this.iframe.frameBorder = 0;
+    this.editor.parentNode.insertBefore(this.iframe, this.editor);
+    //插入设置
+    this.createControl();
+    //获取并设置iframeDocument
+    this.iframeDoc = this.iframe.contentDocument;
+    this.iframeDoc.designMode = 'on';
+    this.iframeDoc.open();
+    this.iframeDoc.write('<html><head><style type="text/css">body{ font-family:arial; font-size:14px;background:#DDF3FF }</style></head></html>');
+    this.iframeDoc.close();
+    
+    this.keyUp(this.iframeDoc, [17, 66], () => {
+      alert('a')
     })
+    
   },
-  reSet (el) {
-    //离开当前内容前检测是否为空
-    el.addEventListener('blur', () => {
-      isNotCont = el.innerText;      
-      if(!isNotCont) {
-        this.removeEl(el);
-      }
-      
-    })
+  createControl () {
+    var mainCont = this.creEl('div');
+    mainCont.style.width = this.data.width;
+    mainCont.style.height = '60px';
+    mainCont.style.backgroundColor = '#69f';
+
+    this.editor.parentNode.insertBefore(mainCont, this.iframe);
   },
-  editCont (el) {
-    el.addEventListener('mousedown', (ev) => {
+  keyUp (el, kCode, callback) {
+    el.onkeyup = (ev) => {
       ev = ev || event;
-      ev.stopPropagation();
-      console.log('a');
-      
-    })
+      var result;
+      if (typeof kCode === 'number' && kCode === ev.keyCode) {
+        result = true;
+      } else if (typeof kCode === 'object') {
+        kCode.forEach(val => {
+          let result = []
+          if (kCode === val) {
+            result.push(true)
+          } else {
+
+          }
+        })
+      } else {
+        result = false;
+      }
+
+      if (result) {
+        callback();
+      }
+    }
   },
   removeEl (el) {
     el.parentNode.removeChild(el);
   },
   creEl (el) {
     return document.createElement(el);
+  },
+  seEl (el) {
+    return document.querySelector(el);
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+var tpl = {
+  name: '粗体',
+  tag: 'button',
+  
+}
+
 new EasyEditor({
-  el: '#editor'
+  el: '#editor',
+  width: '500px',
+  height: '300px'
 });
