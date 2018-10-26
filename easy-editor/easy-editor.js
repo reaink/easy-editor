cafdf17e6b7f07a40403d4372c608d51f3be0681
不书 <nmlixa@163.com>
2018-10-26 17:06:01 +0800

    修复：内容区高度偏差。

--- easy-editor/easy-editor.js
+++ easy-editor/easy-editor.js
@@ -14,15 +14,15 @@ EasyEditor.prototype = {
     //获取并设置editor
     this.editor = this.seEl(this.data.el);
     this.editor.className = 'es-editor';
-    this.editor.style.width = this.data.width;
-    this.editor.style.height = this.data.height;
+    this.editor.style.maxWidth = this.data.width;
+    this.editor.style.minHeight = this.data.height;
 
     //插入设置
     this.createControl();
     //创建iframe
     this.iframe = this.creEl('iframe');
-    this.iframe.style.width = this.data.width;
-    this.iframe.style.height = this.editor.offsetHeight - this.seEl('.es-controls').offsetHeight - 2 + 'px';
+    this.iframe.style.maxWidth = this.data.width;
+    this.iframe.style.height = this.editor.offsetHeight - this.seEl('.es-controls').offsetHeight + 'px';
     this.iframe.frameBorder = 0;
     //插入iframe
     this.editor.appendChild(this.iframe);
@@ -30,15 +30,16 @@ EasyEditor.prototype = {
     this.iframeDoc = this.iframe.contentDocument;
     this.iframeDoc.designMode = 'on';
     this.iframeDoc.open();
-    this.iframeDoc.write('<html><head><style type="text/css">body{ font-family:arial; font-size:14px;background:#DDF3FF }</style></head><body>我是预置的文本我是预置的文本我是预置的文本我是预置的文本我是预置的文本</body></html>');
+    this.iframeDoc.write('<html><head><style type="text/css">body{ font-family:arial; font-size:14px;background:#E0E3DA }</style></head><body>青玉案·元夕宋代：辛弃疾东风夜放花千树。更吹落、星如雨。宝马雕车香满路。凤箫声动，玉壶光转，一夜鱼龙舞。蛾儿雪柳黄金缕。笑语盈盈暗香去。众里寻他千百度。蓦然回首，那人却在，灯火阑珊处。</body></html>');
     this.iframeDoc.close();
     
     this.keyUp(this.iframeDoc, ['ctrlKey', 66], () => {
       this.iframeDoc.execCommand('bold', false, '');
     })
   },
-  // 创建功能区，添加功能按钮
+  //创建功能区，添加功能按钮
   createControl () {
+    //颜色按钮创建函数
     function createColorBtn (color, pos) {
       var el = _this.createBtn('', 'button', 'button', 'click', () => {
         _this.iframeDoc.execCommand(pos, false, color);
@@ -47,18 +48,33 @@ EasyEditor.prototype = {
       el.style.backgroundColor = color;
       return el;
     }
-    
+    //主控制区
     var mainCont = this.creEl('div');
     mainCont.className = 'es-controls';
     mainCont.style.width = this.editor.offsetWidth - 10 + 'px';
     mainCont.style.padding = '5px';
-    mainCont.style.height = '60px';
-    mainCont.style.backgroundColor = '#69f';
+    mainCont.style.minHeight = '60px';
+    mainCont.style.backgroundColor = '#667';
 
+    //设计模式切换
+    var designBtn = this.createBtn('查看', 'button', 'button', 'click', () => {
+      if (this.iframeDoc.designMode === 'on') {
+        this.iframeDoc.designMode = 'off';
+        this.iframeDoc.body.style.backgroundColor = '#eee';
+        designBtn.innerText = '设计';
+      } else {
+        this.iframeDoc.designMode = 'on'
+        this.iframeDoc.body.style.backgroundColor = '#E0E3DA';
+        designBtn.innerText = '查看';
+      }
+    })
+
+    //粗体
     var boldBtn = this.createBtn('粗体', 'button', 'button', 'click', () => {
       this.iframeDoc.execCommand('bold', false, '');
     })
 
+    //斜体
     var italicBtn = this.createBtn('斜体', 'button', 'button', 'click', () => {
       this.iframeDoc.execCommand('italic', false, '');
     })
@@ -135,12 +151,12 @@ EasyEditor.prototype = {
     })
 
     //背景面板
-    var backPlate = this.creEl('div');
-    backPlate.className = 'es-con es-con-color';
-    backPlate.style.display = 'none';
+    var bgPlate = this.creEl('div');
+    bgPlate.className = 'es-con es-con-color';
+    bgPlate.style.display = 'none';
 
-    function setBackPlatePos (backPlate) {
-      backPlate.style.left = '200px';
+    function setBackPlatePos (bgPlate) {
+      bgPlate.style.left = '200px';
     }
     var _this = this;
     var blackBtn = createColorBtn('black', 'hiliteColor');
@@ -154,27 +170,43 @@ EasyEditor.prototype = {
     var cyanBtn = createColorBtn('cyan', 'hiliteColor');
     var whiteBtn = createColorBtn('white', 'hiliteColor');
 
-    backPlate.appendChild(blackBtn);
-    backPlate.appendChild(redBtn);
-    backPlate.appendChild(yellowBtn);
-    backPlate.appendChild(blueBtn);
-    backPlate.appendChild(greenBtn);
-    backPlate.appendChild(pinkBtn);
-    backPlate.appendChild(maroonBtn);
-    backPlate.appendChild(orangeBtn);
-    backPlate.appendChild(cyanBtn);
-    backPlate.appendChild(whiteBtn);
+    bgPlate.appendChild(blackBtn);
+    bgPlate.appendChild(redBtn);
+    bgPlate.appendChild(yellowBtn);
+    bgPlate.appendChild(blueBtn);
+    bgPlate.appendChild(greenBtn);
+    bgPlate.appendChild(pinkBtn);
+    bgPlate.appendChild(maroonBtn);
+    bgPlate.appendChild(orangeBtn);
+    bgPlate.appendChild(cyanBtn);
+    bgPlate.appendChild(whiteBtn);
 
     //背景按钮
-    var backBtn = this.createBtn('背景', 'button', 'button', 'click', () => {
-      if (backPlate.style.display === 'none') {
-        backPlate.style.display = 'block';
+    var bgBtn = this.createBtn('背景', 'button', 'button', 'click', () => {
+      if (bgPlate.style.display === 'none') {
+        bgPlate.style.display = 'block';
       } else {
-        backPlate.style.display = 'none';
+        bgPlate.style.display = 'none';
       }
     })
 
 
+    //链接按钮
+    var linkBtn = this.createBtn('链接', 'button', 'button', 'click', () => {
+      var href = prompt('请输入链接地址：');
+      this.iframeDoc.execCommand('createLink', false, href);
+    })
+
+
+
+    
+    //本项目地址
+    var originBtn = this.createBtn('项目', 'button', 'button', 'click', () => {
+      window.open('https://gitee.com/nshu/EasyEditor');
+    })
+
+
+    mainCont.appendChild(designBtn);
     mainCont.appendChild(boldBtn);
     mainCont.appendChild(italicBtn);
     mainCont.appendChild(fontBtn);
@@ -186,9 +218,12 @@ EasyEditor.prototype = {
     colorBtn.parentNode.appendChild(colorPlate);
     setColorPlatePos(colorPlate);
     //添加设置背景面板、按钮
-    mainCont.appendChild(backBtn);
-    backBtn.parentNode.appendChild(backPlate);
-    setBackPlatePos(backPlate);
+    mainCont.appendChild(bgBtn);
+    bgBtn.parentNode.appendChild(bgPlate);
+    setBackPlatePos(bgPlate);
+    mainCont.appendChild(linkBtn);
+    mainCont.appendChild(originBtn);
+
     this.editor.appendChild(mainCont);
   },
   /**
